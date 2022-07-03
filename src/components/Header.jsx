c8a620a2929bf41ce0ae44c175647edce7330f9f
mikeydgithub <ampgzo2@gmail.com>
2022-07-03 15:12:25 -0400

    fix react-router-dom version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,86 @@
-import React from 'react'
-import classes from './Header.module.scss';
-import {BiMenuAltRight} from 'react-icons/bi'
-import {AiOutlineClose} from 'react-icons/ai'
+import React, { useEffect, useState } from "react";
+
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import classes from "./Header.module.scss";
+import { Link, useHistory } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-        <div className={classes.header_content}>
-            <h2 className={classes.header.logo}>navbar</h2>
-            
-            <nav className={classes.header_content_nav}>
-            <ul>
-                <li>
-                    <a href="/">PageOne</a>
-                </li>
-                <li>
-                    <a href="/">PageTwo</a>
-                </li>
-                <li>
-                    <a href="/">PageThree</a>
-                </li>
-            </ul>
-            <button>CTA Page</button>
-            </nav>
-            <div className={classes.header_content_toggle}>
-                <BiMenuAltRight/>
-            </div>
-        </div>
-    </header>
-  )
-}
+    const history = useHistory ();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [size, setSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
 
-export default Header
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false);
+        }
+    }, [size.width, menuOpen]);
+
+    const menuToggleHandler = () => {
+        setMenuOpen((p) => !p);
+    };
+
+    const ctaClickHandler = () => {
+        menuToggleHandler();
+        history.push("/page-cta");
+    };
+
+    return (
+        <header className={classes.header}>
+            <div className={classes.header__content}>
+                <Link to="/" className={classes.header__content__logo}>
+                    navbar
+                </Link>
+                <nav
+                    className={`${classes.header__content__nav} ${
+                        menuOpen && size.width < 768 ? classes.isMenu : ""
+                    }`}
+                >
+                    <ul>
+                        <li>
+                            <Link to="/page-one" onClick={menuToggleHandler}>
+                                PageOne
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/page-two" onClick={menuToggleHandler}>
+                                PageTwo
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/page-three" onClick={menuToggleHandler}>
+                                PageThree
+                            </Link>
+                        </li>
+                    </ul>
+                    <button onClick={ctaClickHandler}>CTA Page</button>
+                </nav>
+                <div className={classes.header__content__toggle}>
+                    {!menuOpen ? (
+                        <BiMenuAltRight onClick={menuToggleHandler} />
+                    ) : (
+                        <AiOutlineClose onClick={menuToggleHandler} />
+                    )}
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/pages/PageTwo.jsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+const PageTwo = () => {
+    return <h1>Page Two</h1>;
+};
+
+export default PageTwo;
