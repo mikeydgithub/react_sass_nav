535aa25e2f589d5f2fb2d52f18c9ff60ae308915
mikeydgithub <ampgzo2@gmail.com>
2022-07-03 12:50:49 -0400

    commiting quick. thunderstorm approaching

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,24 +7,25 @@ const Header = () => {
   return (
     <header className={classes.header}>
         <div className={classes.header_content}>
-            <h2>navbar</h2>
-        </div>
-        <nav className={classes.header_content_nav}>
-        <ul>
-            <li>
-                <a href="/">PageOne</a>
-            </li>
-            <li>
-                <a href="/">PageTwo</a>
-            </li>
-            <li>
-                <a href="/">PageThree</a>
-            </li>
-        </ul>
-        <button>CTA Page</button>
-        </nav>
-        <div className={classes.header_content_toggle}>
-            <BiMenuAltRight/>
+            <h2 className={classes.header.logo}>navbar</h2>
+            
+            <nav className={classes.header_content_nav}>
+            <ul>
+                <li>
+                    <a href="/">PageOne</a>
+                </li>
+                <li>
+                    <a href="/">PageTwo</a>
+                </li>
+                <li>
+                    <a href="/">PageThree</a>
+                </li>
+            </ul>
+            <button>CTA Page</button>
+            </nav>
+            <div className={classes.header_content_toggle}>
+                <BiMenuAltRight/>
+            </div>
         </div>
     </header>
   )
